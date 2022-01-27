0d46f5af3ba295251121c4f2bbcb61e40451f616
Baroshem <jakub.andrzejewski.dev@gmail.com>
2022-01-27 10:04:49 +0100

    feat: add storyblok

--- app.vue
+++ app.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <p v-for="story in stories" :key="story.id">{{ story.name }}</p>
     {{ result }}
     <NuxtWelcome />
   </div>
@@ -8,7 +9,12 @@
 <script setup>
 const { result, search } = useSearch('test_index') // pass your index as param
 
+const storyapi = useStoryApi();
+const { data } = await storyapi.get("cdn/stories", { version: "draft" });
+const state = reactive({ stories: data.story });
+
 onMounted(async () => {
   await search({ query: 'Samsung' });
+  useStoryBridge(state.story.id, story => (state.story = story));
 })
 </script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,9 +3,12 @@ import { defineNuxtConfig } from 'nuxt3'
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
   buildModules: [
-    '@nuxt-modules/algolia', {
+    ['@nuxt-modules/algolia', {
       apiKey: '<YOUR_SEARCH_API_KEY>',
       applicationId: '<YOUR_APPLICATION_ID>'
-    }
+    }],
+    ['@storyblok/nuxt', {
+      accessToken: "YOUR_ACCESS_TOKEN"
+    }]
   ]
 })
