0e280becceea854b8989cb493216bb967d96970a
Baroshem <jakub.andrzejewski.dev@gmail.com>
2022-01-27 09:36:21 +0100

    feat: add algolia

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,10 @@ import { defineNuxtConfig } from 'nuxt3'
 
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
-
+  buildModules: [
+    '@nuxt-modules/algolia', {
+      apiKey: '<YOUR_SEARCH_API_KEY>',
+      applicationId: '<YOUR_APPLICATION_ID>'
+    }
+  ]
 })
