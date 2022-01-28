f72f0914afeef16ccfb3b5279c45ff6b5451be87
Baroshem <jakub.andrzejewski.dev@gmail.com>
2022-01-28 11:35:04 +0100

    feat: AlgoliaResult component

---
+++ components/AlgoliaResult.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h2>Result from Algolia</h2>
+    <p>{{ result }}</p>
+  </div>
+</template>
+
+
+<script setup lang="ts">
+const { result, search } = useSearch('storyblok')
+
+onMounted(async () => {
+  await search({ query: 'Test' });
+})
+</script>
