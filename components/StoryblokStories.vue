5bbb7e9118a08c78e7999938b744a447fcfac783
Baroshem <jakub.andrzejewski.dev@gmail.com>
2022-01-28 11:35:13 +0100

    feat: StoryblokStories component

---
+++ components/StoryblokStories.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h2>Stories from Storyblok</h2>
+    <pre>{{ state }}</pre>
+  </div>
+</template>
+
+<script setup lang="ts">
+const storyapi = useStoryApi();
+const { data } = await storyapi.get('cdn/stories', { version: 'draft' });
+const state = reactive({ stories: data.stories });
+</script>
