48b1a4165133067e44e1507b15fc9c3e7b8172d1
Baroshem <jakub.andrzejewski.dev@gmail.com>
2022-03-01 13:00:53 +0100

    feat: add @nuxtjs/algolia

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,7 +6,7 @@ export default defineNuxtConfig({
     transpile: ['#app'] // Temporary workaround for issues in Storyblok module
   },
   buildModules: [
-    ['@nuxt-modules/algolia', {
+    ['@nuxtjs/algolia', {
       apiKey: process.env.ALGOLIA_SEARCH_API_KEY,
       applicationId: process.env.ALGOLIA_APP_ID,
     }],
